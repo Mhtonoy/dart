0c686e24bbfbecd4395c316f9046298eab07c07a
Mozammal Haque Tonoy <realtonoy@gmail.com>
2025-04-25 21:38:14 +0600

    For loop related programs (commit#11)

---
+++ forloop/bin/forloop.dart
@@ -0,0 +1,65 @@
+/*
+/// Exaple 1
+// Print Name 10 Times Using For Loop
+void main(){
+  for(int i=0; i<10; i++){
+    print("John Doe");
+  }
+}
+*/
+
+/*
+/// Example 2
+// Print 1 To 10 Using For Loop
+void main(){
+  for(int i=1; i<=10; i++){
+    print(i);
+  }
+} 
+*/
+
+/*
+/// Example 3
+// Print 10 to 1 Using For Loop
+void main(){
+  for(int i=10; i >= 1; i--){
+    print(i);
+  }
+}
+*/
+
+/*
+/// Example 4
+// Display Sum of n Natural Numbers
+void main(){
+  int total = 0;
+  int n = 100;
+  for(int i=1; i<=100; i++){
+    total = total + i;
+  }
+  print("Total is $total");
+}
+*/
+
+/*
+/// Example 5
+// Display Even Numbers Between 50 to 100 
+void main(){
+  for(int i=50; i<=100; i++){
+    if(i%2 == 0){
+      print(i);
+    }
+  }
+}
+*/
+
+/*
+/// Infinite Loop In Dart
+// If the condition never becomes false in looping, it is called an infinite loop.
+// This program prints 1 to infinite.
+void main(){
+  for(int i=1; i>=1; i++){
+    print(i);
+  }
+}
+*/
