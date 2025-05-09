b5c9a1c630fbb478b007d6f85b8faf5a369873cd
Mozammal Haque Tonoy <realtonoy@gmail.com>
2025-05-09 20:58:16 +0600

    While loop related programs (commit#13)

---
+++ whileloop/bin/whileloop.dart
@@ -0,0 +1,51 @@
+/*
+/// Example 1: To Print 1 To 10 Using While Loop
+void main(){
+  int i=1;
+
+  while(i<=10){
+    print(i);
+    i++;
+  }
+}
+*/
+
+/*
+/// Example 2: To Print 10 To 1 Using While Loop
+void main(){
+  int i=10;
+  while(i>=1){
+    print(i);
+    i--;
+  }
+}
+*/
+
+/*
+/// Example 3: Display Sum of n Natural Numbers Using While Loop
+void main(){
+  int total = 0;
+  int n = 100;
+  int i = 1;
+
+  while(i<=100){
+    total = total + i;
+    i++;
+  }
+
+  print("Total is $total");
+}
+*/
+
+/*
+/// Example 4: Display Even Numbers Between 50 to 100 Using While Loop
+void main(){
+  int i=50;
+  while(i<=100){
+    if(i%2 == 0){
+      print(i);
+    }
+    i++;
+  }
+}
+*/
