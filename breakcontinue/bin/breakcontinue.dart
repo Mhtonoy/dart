e4a0425628c547f520ad19c95ee6da04154600e1
Mozammal Haque Tonoy <realtonoy@gmail.com>
2025-05-09 21:02:31 +0600

    Use cases of break and continue (commit#14)

---
+++ breakcontinue/bin/breakcontinue.dart
@@ -0,0 +1,125 @@
+/*
+/// Example 1: Break In Dart For Loop
+void main(){
+  for(int i=1; i<=10; i++){
+    if(i==5){
+      break;
+    }
+    print(i);
+  }
+}
+*/
+
+/*
+/// Example 2: Break In Dart Negative For Loop
+void main(){
+  for(int i=10; i>=1; i--){
+    if(i==6){
+      break;
+    }
+    print(i);
+  }
+}
+*/
+
+/*
+/// Example 3: Break In Dart While Loop
+void main(){
+  int i=1;
+  while(i<=10){
+    if(i==6){
+      break;
+    }
+    print(i);
+    i++;
+  }
+}
+*/
+
+/*
+/// Break In Switch Case
+void main(){
+  int noOfMonth = 5;
+
+  switch(noOfMonth){
+    case 1:
+        print("The selected month is January.");
+        break;
+    case 2:
+        print("The selected month is Fabruary.");
+        break;
+    case 3:
+        print("The selected month is March.");
+        break;
+    case 4:
+        print("The selected month is April.");
+        break;
+    case 5:
+        print("The selected month is May.");
+        break;
+    case 6:
+        print("The selected month is June.");
+        break;
+    case 7:
+        print("The selected month is July.");
+        break;
+    case 8:
+        print("The selected month is August.");
+        break;
+    case 9:
+        print("The selected month is September.");
+        break;
+    case 10:
+        print("The selected month is October.");
+        break;
+    case 11:
+        print("The selected month is November.");
+        break;
+    case 12:
+        print("The selected month is December.");
+        break;
+    default:
+        print("Invalid Month.");
+        break;
+  }
+}
+*/
+
+/*
+/// Example 5: Continue In Dart
+void main(){
+  for(int i=1; i<=10; i++){
+    if(i==5){
+      continue;
+    }
+    print(i);
+  }
+}
+*/
+
+/*
+/// Example 6: Continue In Negative For Loop
+void main(){
+  for(int i=10; i>=1; i--){
+    if(i==4){
+      continue;
+    }
+    print(i);
+  }
+}
+*/
+
+/*
+/// Example 7: Continue In While Loop
+void main(){
+  int i=1;
+  while(i<=10){
+    if(i==6){
+      i++;
+      continue;
+    }
+    print(i);
+    i++;
+  }
+}
+*/
